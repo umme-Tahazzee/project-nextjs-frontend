1a79778647e6dddd35c4419ebae8fb28b948a91b
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-07-18 20:41:33 +0600

    Integrating ShadcnU

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
-      
+        <Button>Click</Button>
     </div>
   );
 }
